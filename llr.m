b0a54c09f37dfe6ff0d9141269266e5fb8606195
Irwansyah <109840077+iwplus@users.noreply.github.com>
2023-07-14 08:21:06 +0800

    Update llr.m

--- llr.m
+++ llr.m
@@ -1,6 +1,6 @@
 function b = llr(yi,t,n);
       if yi == 1,
-        b = log(t/(n-t)); ### nilai LLR untuk Binary symmetric channel
+        b = log(t/(n-t)); ### LLR value in Binary symmetric channel
       else,
         b = log((n-t)/t); ############################################
       endif
